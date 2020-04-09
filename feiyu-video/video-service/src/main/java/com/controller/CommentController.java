dc5ad843ab205b64871f3691562b43b065f30c7d
xiake <15813384515@163.com>
2020-04-10 00:19:26 +0800

    视频模块更新

--- feiyu-video/video-service/src/main/java/com/controller/CommentController.java
+++ feiyu-video/video-service/src/main/java/com/controller/CommentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *
  * @Author: xiake
  * @Date: 2020/4/9 20:38
  * @Description:
